4be6899823160ebffd71266b6b82599be7414443
qwertycxz <gmail2@qq.com>
2024-02-02 18:55:02 +0800

    日数据预处理

--- 数据预处理.r
+++ 数据预处理.r
@@ -26,14 +26,31 @@ for (i in 1995:2022) {
     ontario_mean_surface_water_temperature[index, ] <- data_frame$Ont.
 }
 # TODO: 日数据
-data_frame <- read.csv("https://www.glerl.noaa.gov/pubs/tech_reports/glerl-083/UpdatedFiles/daily/subdata_eri_basn.csv", skip = 8)
-data_frame <- cbind(as.integer(format(as.Date(data_frame[, 1]), "%Y")), data_frame)
-
-for (i in 1940:2021) {
-    data_frame_per_year <- data_frame[data_frame[, 1] == i, ]
-    if (sum(data_frame[, 1] == i) == 365) {
-        data_frame_per_year <- rbind(data_frame[1:59, ], NA, data_frame[60:365, ])
+getGlerlData <- function(path) {
+    data_frame <- read.csv(paste0("https://www.glerl.noaa.gov/pubs/tech_reports/glerl-083/UpdatedFiles/", path), skip = 8)
+    data_frame <- cbind(format(as.Date(data_frame[, 1]), "%Y"), data_frame)
+    min_air_temperature <- matrix(dimnames = list(1940:2021, 1:366), ncol = 366, nrow = 82)
+    max_air_temperature <- min_air_temperature
+    mean_precipitation <- min_air_temperature
+    mean_air_temperature <- min_air_temperature
+    mean_dewpoint <- min_air_temperature
+    mean_wind_speed <- min_air_temperature
+    mean_cloud_cover <- min_air_temperature
+    for (i in 1940:2021) {
+        index <- as.character(i)
+        data_frame_per_year <- data_frame[data_frame[, 1] == index, ]
+        if (sum(data_frame[, 1] == index) == 365) {
+            data_frame_per_year <- rbind(data_frame[1:59, ], NA, data_frame[60:365, ])
+        }
+        len <- seq_len(nrow(data_frame_per_year))
+        min_air_temperature[index, len] <- data_frame_per_year[, 3]
+        max_air_temperature[index, len] <- data_frame_per_year[, 4]
+        mean_precipitation[index, len] <- data_frame_per_year[, 5]
+        mean_air_temperature[index, len] <- data_frame_per_year[, 6]
+        mean_dewpoint[index, len] <- data_frame_per_year[, 7]
+        mean_wind_speed[index, len] <- data_frame_per_year[, 8]
+        mean_cloud_cover[index, len] <- data_frame_per_year[, 9]
     }
-    rbind(erie_basin_mean_air_temperature, data_frame_per_year[, 3])
+    return(list(min_air_temperature, max_air_temperature, mean_precipitation, mean_air_temperature, mean_dewpoint, mean_wind_speed, mean_cloud_cover))
 }
-superior_basin_min_air_temperature <- getOfficalData("Superior Min Air Temp")
+superior_basin_daliy <- getGlerlData("daily/subdata_eri_basn.csv")
