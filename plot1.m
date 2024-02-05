aa09e2ca64238ab9c0999f375b612e4838e39f6f
qwertycxz <gmail2@qq.com>
2024-02-05 10:11:59 +0800

    修改热土颜色

--- plot1.m
+++ plot1.m
@@ -5,5 +5,5 @@ plotHeatMap('冬')
 function plotHeatMap(season)
 	values = ["Erie" "Huron" "Michigan" "Ontario" "Superior"];
 	display_data = ["Superior" "Michigan" "Huron" "Erie" "Ontario"];
-	heatmap(figure(Name = season), values, values, table2array(readtable("交互性.xlsx", ReadRowNames = true, Sheet = season)), XDisplayData = display_data, YDisplayData = display_data)
+	heatmap(figure(Name = season), values, values, table2array(readtable("交互性.xlsx", ReadRowNames = true, Sheet = season)), MissingDataColor = 'w', XDisplayData = display_data, YDisplayData = display_data)
 end
